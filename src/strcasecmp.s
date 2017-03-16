e907a7d9e9771b67f24247f5664b295bffb62a4b
Lucas TRONCY <lucas.troncy@epitech.eu>
2017-03-16 23:16:26 +0100

    fixed strcasecmp

--- src/strcasecmp.s
+++ src/strcasecmp.s
@@ -10,16 +10,55 @@ _strcasecmp_loop:
         mov byte al, [rdi + rcx] ; Get s1 character
         mov byte ah, [rsi + rcx] ; Get s2 character
 
-        cmp 0, al		 ; Test null byte
+        cmp byte al, 0		 ; Test null byte
         je _strcasecmp_loop_end
-        cmp 0, ah
+        cmp byte ah, 0
         je _strcasecmp_loop_end
 
-	or al, 32		; Set to lowercase
-        or ah, 32		; Set to lowercase
-        cmp al, ah
+        mov dl, al
+        mov dh, ah
+        or dl, 32
+        or dh, 32
+        cmp dl, dh
+        jne _strcasecmp_loop_end
+
+_strcasecmp_continue_loop:
+        inc rcx
+        jmp _strcasecmp_loop
 
 _strcasecmp_loop_end:
+        cmp al, 'A'
+        jl _strcasecmp_real_end
+        cmp al, 'z'
+        jg _strcasecmp_real_end
+        cmp al, 'a'
+        jge _strcasecmp_letter
+        cmp al, 'Z'
+        jg _strcasecmp_real_end
+
+_strcasecmp_letter:
+        cmp ah, 'A'
+        jl _strcasecmp_real_end
+        cmp ah, 'z'
+        jg _strcasecmp_real_end
+        cmp ah, 'a'
+        jge _strcasecmp_letter_ok
+        cmp ah, 'Z'
+        jg _strcasecmp_real_end
+        
+_strcasecmp_letter_ok:
+        or al, 32
+        or ah, 32
+        
+_strcasecmp_real_end:
+        xor rcx, rcx
+        mov ch, al
+        mov cl, ah
+        xor rax, rax
+        mov al, ch
+        xor ch, ch
+        sub rax, rcx
+        ret
 
 %elifidn __OUTPUT_FORMAT__, elf32
 		[BITS 32]
